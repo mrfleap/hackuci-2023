f0d2b634e10dbfabca116c6f33eb13b4f3e892ae
Ryan Zhang <zyratlo@gmail.com>
2023-02-04 13:17:36 -0800

    modified to call on department, not course name

--- schedule/class_info.py
+++ schedule/class_info.py
@@ -9,44 +9,46 @@ import json
 # Location
 # Time
 
-def getClassInfo(year: int, quarter: str, department: str, courseNumber: int) -> dict[int: list[dict]]:
-    response = requests.get(f"https://api.peterportal.org/rest/v0/schedule/soc?term={year}%20{quarter}&department={department}&courseNumber={courseNumber}").content.decode("utf-8")
+def getClassInfo(year: int, quarter: str, department: str) -> dict[int: list[dict]]:
+    response = requests.get(f"https://api.peterportal.org/rest/v0/schedule/soc?term={year}%20{quarter}&department={department}").content.decode("utf-8")
     if response == '{"schools":[]}' : return {}
     response = json.loads(response)
     response = getInfo(response)
-    return {f"{department} {courseNumber}": response}
+    return response
 
-def getInfo(response: dict) -> list[dict]:
-    info = []
-
-    for section in response["schools"][0]["departments"][0]["courses"][0]["sections"]:
-        sectionType = section["sectionType"]
-        sectionNum = section["sectionNum"]
-        units = section["units"]
-        finalExam = section["finalExam"]
-        course_id = section["sectionCode"]
-        professors = section["instructors"]
-        location = section["meetings"][0]["bldg"]
-        course_time = {"days": section["meetings"][0]["days"], "time": section["meetings"][0]["time"]}
-        info.append({"ID": course_id, "sectionType": sectionType, "sectionNum": sectionNum, "units": units, "finalExam": finalExam, "professors": professors, "location": location, "time": course_time})
-
-        print(course_id)
-        # time = Course_Time(False, False, False, False, False, 0, 0)
-        # if "M" in section["meetings"][0]["days"]:
-        #     time.mon = True
-        # if "Tu" in section["meetings"][0]["days"]:
-        #     time.tue = True
-        # if "W" in section["meetings"][0]["days"]:
-        #     time.wed = True
-        # if "Th" in section["meetings"][0]["days"]:
-        #     time.thu = True
-        # if "F" in section["meetings"][0]["days"]:
-        #     time.fri = True
-        # times = convert_time(section["meetings"][0]["time"])
-        # time.start = times[0]
-        # time.end = times[1]
-        # info.append(Course(course_name, course_id, professors, time, location))
-
+def getInfo(response: dict) -> dict[str: list[dict]]:
+    info = {}
+    for course in response["schools"][0]["departments"][0]["courses"]:
+        sections = []
+        print(f"\tProcessing {course['deptCode']} {course['courseNumber']}")
+        for section in course["sections"]:
+            print(f"\t\tProcessing {section['sectionCode']}")
+            # print(section)
+            sectionType = section["sectionType"]
+            sectionNum = section["sectionNum"]
+            units = section["units"]
+            finalExam = section["finalExam"]
+            course_id = section["sectionCode"]
+            professors = section["instructors"]
+            location = section["meetings"][0]["bldg"]
+            course_time = {"days": section["meetings"][0]["days"], "time": section["meetings"][0]["time"]}
+            sections.append({"ID": course_id, "sectionType": sectionType, "sectionNum": sectionNum, "units": units, "finalExam": finalExam, "professors": professors, "location": location, "time": course_time})
+            # time = Course_Time(False, False, False, False, False, 0, 0)
+            # if "M" in section["meetings"][0]["days"]:
+            #     time.mon = True
+            # if "Tu" in section["meetings"][0]["days"]:
+            #     time.tue = True
+            # if "W" in section["meetings"][0]["days"]:
+            #     time.wed = True
+            # if "Th" in section["meetings"][0]["days"]:
+            #     time.thu = True
+            # if "F" in section["meetings"][0]["days"]:
+            #     time.fri = True
+            # times = convert_time(section["meetings"][0]["time"])
+            # time.start = times[0]
+            # time.end = times[1]
+            # info.append(Course(course_name, course_id, professors, time, location))
+        info.update({f"{course['deptCode']} {course['courseNumber']}": sections})
     return info
 
 def convert_time(time: str) -> list[int, int]:
@@ -82,28 +84,45 @@ def getAllCourseNames() -> None:
             f.write(f"{course.get('department', None)} {course.get('number', None)}\n")
 
 
-with open("all_course_names.txt", "r") as fin:
-    # counter = 0
+def getDepartments():
+    departments = set()
+    with open("all_course_names.txt", "r") as f:
+        for line in f:
+            departments.add(line.rsplit(" ", 1)[0])
+    return departments
+
+if __name__ == "__main__":
     all_class = {}
-
-    for line in fin:
-        course = line.strip().rsplit(sep=" ", maxsplit=1)
-        time.sleep(0.1)
-        print(f"Retrieving {course[0]} {course[1]} information...")
-        course[0] = course[0].replace("&", "%26")
-        course[0] = course[0].replace("/", "%2F")
-        all_class.update(getClassInfo(2023, "Winter", course[0], course[1]))
-        print()
-
-        # counter += 1
-        # if counter == 6:
-        #     break
+    for department in getDepartments():
+        time.sleep(0.5)
+        print(f"Processing {department}...")
+        all_class.update(getClassInfo(2023, "Winter", department))
 
     with open("all_course_info.json", "w") as fout:
         json.dump(all_class, fout)
+
+    # with open("all_course_names.txt", "r") as fin:
+    #     # counter = 0
+    #     all_class = {}
+
+    #     for line in fin:
+    #         course = line.strip().rsplit(sep=" ", maxsplit=1)
+    #         time.sleep(0.1)
+    #         print(f"Retrieving {course[0]} {course[1]} information...")
+    #         course[0] = course[0].replace("&", "%26")
+    #         course[0] = course[0].replace("/", "%2F")
+    #         all_class.update(getClassInfo(2023, "Winter", course[0], course[1]))
+    #         print()
+
+    #         # counter += 1
+    #         # if counter == 6:
+    #         #     break
+
+    #     with open("all_course_info.json", "w") as fout:
+    #         json.dump(all_class, fout)
     
-# course = ["CRM/LAW", "C7"]
-# course[0] = course[0].replace("&", "%26")
-# course[0] = course[0].replace("/", "%2F")
-# print(course[0])
-# print(getClassInfo(2023, "Winter", course[0], course[1]))
+    # course = ["CRM/LAW", "C7"]
+    # course[0] = course[0].replace("&", "%26")
+    # course[0] = course[0].replace("/", "%2F")
+    # print(course[0])
+    # print(getClassInfo(2023, "Winter", course[0], course[1]))
