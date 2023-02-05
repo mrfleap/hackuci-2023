be8d822738e447d5ecedced60b04f282ece2ebb8
Nathan Monette <nathanmonette1@gmail.com>
2023-02-04 17:05:58 -0800

    Started C Programming Schedule Generation

---
+++ schedule/generate.hpp
@@ -0,0 +1,21 @@
+#ifndef GENERATE
+#define GENERATE
+
+#include <vector>
+#include <string>
+
+struct Course {
+    std::string name;
+    std::string id;
+};
+
+class Schedule {
+    private:
+        std::vector<Course> courses;
+
+    public:
+
+    
+};
+
+#endif
